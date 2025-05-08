f142d5e805b500a265760aaa9f8d44ec434a2729
jorgendo43 <jorge.verduzco247253@potros.itson.edu.mx>
2025-05-08 01:38:31 -0700

    Proyectos iniciales

---
+++ PersistenceHealthRecord/src/main/java/IDaoHealthRecord/ITutorDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package IDaoHealthRecord;
+
+/**
+ *
+ * @author JORGE
+ */
+public interface ITutorDAO {
+    
+}
