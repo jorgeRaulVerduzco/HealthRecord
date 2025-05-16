c764d711d39d8c9834dba9b9a86383b3b2d1983d
jorgendo43 <jorge.verduzco247253@potros.itson.edu.mx>
2025-05-16 00:53:40 -0700

    Expedien con front y back listo

--- ExpedientService/src/main/java/ExpedientService/WebConfig.java
+++ ExpedientService/src/main/java/ExpedientService/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,5 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
    registry.addMapping("/**")
         .allowedOrigins("*")
         .allowedMethods("*");
+  }
+   @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    // Ajusta esta ruta al folder donde están tus .html, styles/, imgs/, etc.
+    String frontPath = "file:///C:/Users/JORGE/OneDrive/Documentos/NetBeansProjects/HealthRecordProject/HealthCareWebApp/src/main/webapp/";
+
+    registry.addResourceHandler("/**")              // todas las peticiones estáticas
+            .addResourceLocations(frontPath)        // ruta de tu front en tu disco
+            .setCachePeriod(0);                     // en desarrollo no cachea
   }
 }
