87de0de3d974bde1e202c9157a0c74a43d707969
jorgendo43 <jorge.verduzco247253@potros.itson.edu.mx>
2025-05-16 00:32:25 -0700

    Avances
    se agregaron rutas y mas para hacer el rest

---
+++ ExpedientService/src/main/java/ExpedientService/WebConfig.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ExpedientService;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+   registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("*");
+  }
+}
