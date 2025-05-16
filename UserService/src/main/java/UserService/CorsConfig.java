aabee390c3e1bf38cba6c5490d8ef4426a8389f2
Yalam14 <jesus.guzman233350@potros.itson.edu.mx>
2025-05-16 14:59:40 -0700

    Autenticacion con el front "Tienen que correr primero UseService poder correrlo chequen descripcion les paso la ruta "
    http://localhost:8082/HealthCareWebApp/

---
+++ UserService/src/main/java/UserService/CorsConfig.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UserService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:8082") // frontend en GlassFish
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
--- UserService/src/main/java/UserService/HealthcareApplication.java
+++ UserService/src/main/java/UserService/HealthcareApplication.java
@@ -16,10 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EntityScan(basePackages = "Entities") 
 public class HealthcareApplication {
     public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    String rawPassword = "12345";
-    String encodedPassword = encoder.encode(rawPassword);
-    System.out.println("Nuevo hash para 12345: " + encodedPassword);
+        
         SpringApplication.run(HealthcareApplication.class, args);
     }
 }
